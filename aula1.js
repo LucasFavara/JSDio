07d38a5d6d1d7d6495be9431dfc2977ffae849fc
Lucas Favarão <135760198+LucasFavara@users.noreply.github.com>
2023-08-26 14:45:40 -0300

    Initial commit

---
+++ aula1.js
@@ -0,0 +1,7 @@
+const combustivel = 4.79;
+const gasto = 12;
+const distancia = 1057;
+
+var resultado = distancia / gasto * combustivel;
+
+console.log(`Foram gastos ${resultado.toFixed(2)} reais`);
