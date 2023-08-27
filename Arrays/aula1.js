5b9f922d66f1340dce2af6563afd8e27cf986993
Lucas Favarão <135760198+LucasFavara@users.noreply.github.com>
2023-08-27 15:54:29 -0300

    aula de array

---
+++ Arrays/aula1.js
@@ -0,0 +1,54 @@
+// const alunos = ['Lucas','João','Marcos'];
+
+// alunos.push('Bruno');
+
+// alunos[4] = 'Henrique';
+
+// console.log(alunos);
+
+//_____________________________________________________________
+
+// const notas = [];
+
+// notas.push(5);
+// notas.push(3);
+// notas.push(7);
+// notas.push(8);
+// notas.push(8);
+// notas.push(9);
+
+// const soma = notas[0] + notas[1] + notas[2] + notas[3] + notas[4] + notas[5];
+
+// console.log(soma / notas.length);
+
+//__________________________________________________________________________________
+
+// const notas = [];
+
+// notas.push(5);
+// notas.push(3);
+// notas.push(7);
+// notas.push(8);
+// notas.push(8);
+// notas.push(9);
+
+// const soma = notas[0] + notas[1] + notas[2] + notas[3] + notas[4] + notas[5];
+
+// for (let i = 0; i < 10; i++) {
+//   console.log(i);  
+// }
+
+// console.log(soma / notas.length);
+
+//________________________________________________________________________________
+
+const nome = 'Lucas Nunes Favarão';
+
+// for (let i = 0; i < nome.length; i++) {
+//      console.log(nome[i]);
+//   }
+
+for (let i = 0; i < nome.length; i++) {
+    const letra = nome[i];
+    console.log(letra);
+}
