2105b0759dfa3ffafc185f6185eb44815cad9590
Lucas Favarão <135760198+LucasFavara@users.noreply.github.com>
2023-08-27 17:32:09 -0300

    exercicio de numeros pares

---
+++ Arrays/aula3exercicio.js
@@ -0,0 +1,5 @@
+var multiplicador = 7
+
+for (let i = 0; i < 11; i++) {
+    console.log(i, 'x', multiplicador, '=', i * multiplicador);
+}
---
+++ Arrays/aula4exercicio.js
@@ -0,0 +1,9 @@
+const numeros = [0,1,2,3,4,5,6,7,8,9,10]
+
+for (let i = 0; i < numeros.length; i++) {
+    const par = numeros[i];
+    if (par % 2 === 0) {
+        console.log(par);
+    }   
+}
+
