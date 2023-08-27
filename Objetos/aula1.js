9983f8ac1abccc63279fa400ba248bf305117480
Lucas Favarão <135760198+LucasFavara@users.noreply.github.com>
2023-08-27 11:26:49 -0300

    aula sobre objetos

---
+++ Objetos/aula1.js
@@ -0,0 +1,16 @@
+const lucas = {
+    nome: "Lucas N Favarel",
+    idade: 27,
+
+    descrever: function () {
+        console.log(`Meu nome é ${this.nome} e minha idade é ${this.idade}`);
+    }
+};
+
+lucas.descrever();
+
+// lucas.altura = 1.85;
+
+// delete lucas.nome;
+
+// console.log(lucas);
