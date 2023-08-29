34adf0381e569bdef5c64761299e2bffbbe2aa44
Lucas Favarão <135760198+LucasFavara@users.noreply.github.com>
2023-08-28 22:34:33 -0300

    atualização arrow function

--- Pokedex/Assets/js/main.js
+++ Pokedex/Assets/js/main.js
@@ -3,4 +3,19 @@
 
 const offset = 0
 const limit = 10
-const url = 'https://pokeapi.co/api/v2/pokemon?offset=0&limit=10'
+const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+
+fetch(url)
+    .then((response) => response.json())
+    .then((jsonBody) => console.log(jsonBody))
+    .catch((error) => console.log(error))
+
+//     .then(function(response) {
+//         console.log(response);
+// })
+//     .catch(function (error) {
+//     console.log(error);
+// })
+// .finally(function (){
+//     console.log('Requisição concluida');
+// })
